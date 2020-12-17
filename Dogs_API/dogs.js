c01b0eac74d499a37db39abec23f74ce53481ecc
aelouazz <aelouazz@github.com>
2020-12-17 09:06:18 +0100

    fetching and display of data from API done

--- Dogs_API/dogs.js
+++ Dogs_API/dogs.js
@@ -1,14 +1,52 @@
 const DOG_URL = "https://dog.ceo/api/breeds/image/random";
 
-const promise = fetch(DOG_URL);
+const doggos = document.querySelector(".doggos");
 
-promise
-	.then(response => {
-		const processingPromise = response.json();
-		return processingPromise;
-	})
-	.then(processedResponse => 1{
-		console.log(processedResponse);
-	});
+function addNewDoggo() {
+  const promise = fetch(DOG_URL);
+  promise
+    .then(function(response) {
+      const processingPromise = response.json();
+      return processingPromise;
+    })
+    .then(function(processedResponse) {
+      const img = document.createElement("img");
+      img.src = processedResponse.message;
+	  img.alt = "Cute doggo";
+	  img.className = "dog-image"
+	//   doggos.appendChild(img);
+      doggos.insertBefore(img, doggos.childNodes[0]);
+    });
+}
 
-console.log("this will log first");
+// addNewDoggo();
+// addNewDoggo();
+// addNewDoggo();
+// addNewDoggo();
+// addNewDoggo();
+// addNewDoggo();
+// addNewDoggo();
+// addNewDoggo();
+// addNewDoggo();
+// addNewDoggo();
+// addNewDoggo();
+// addNewDoggo();
+// addNewDoggo();
+// addNewDoggo();
+// addNewDoggo();
+// addNewDoggo();
+
+
+// window.onscroll = function(ev) {
+//     if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight ) {
+// 		addNewDoggo();
+// 		addNewDoggo();
+// 		addNewDoggo();
+// 		addNewDoggo();
+//     }
+// };
+
+document.querySelector(".add-doggo").addEventListener("click", addNewDoggo);
+// document.querySelector(".load").onshow = addNewDoggo();
+// window.onscroll = function() {addNewDoggo()};
+
